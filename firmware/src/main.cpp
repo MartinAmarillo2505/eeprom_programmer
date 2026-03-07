f322be045497d7fdcd21f3c1c7dc67e584967a61
Martín Amarillo <46434353+MartinAmarillo2505@users.noreply.github.com>
2026-03-07 02:30:05 -0300

    chore(firmware): remove unused code

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,34 +3,6 @@
 
 uint8_t cmd[PACKET_MAX_LENGTH];
 
-void eraseEEPROM() {
-  Serial.print(F("Erasing EEPROM"));
-  for (int address = 0; address < 2048; address++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    writeEEPROM(address, 0xFF);
-    digitalWrite(LED_BUILTIN, LOW);
-
-    if (address % 64 == 0) {
-      Serial.print(F("."));
-    }
-  }
-  Serial.println(F(" OK"));
-}
-
-void printContents() {
-  for (int base = 0; base < 128; base++) {
-    uint8_t data[16];
-    readChunkEEPROM(base, data, sizeof(data));
-
-    char msg[60];
-    sprintf_P(msg, PSTR("%03x: %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x"),
-              base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-              data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
-
-    Serial.println(msg);
-  }
-}
-
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   initEEPROM();
